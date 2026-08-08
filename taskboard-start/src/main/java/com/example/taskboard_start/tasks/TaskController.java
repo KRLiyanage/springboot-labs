b6e6c37a331f4f241eb68c4c19fbc4b1324b5af3
KRLiyanage <kaweeshravishanka8@gmail.com>
2026-08-08 21:12:43 +0530

    Add TaskController.java

--- taskboard-start/src/main/java/com/example/taskboard_start/tasks/TaskController.java
+++ taskboard-start/src/main/java/com/example/taskboard_start/tasks/TaskController.java
@@ -1,10 +1,9 @@
 package com.example.taskboard_start.tasks;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tasks")
@@ -20,6 +19,26 @@ public class TaskController {
     @GetMapping
     public List<Task> getAll(){
         return taskService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Task> getById(@PathVariable Long id){
+        return taskService.findById(id);
+    }
+
+    @PostMapping
+    public Task save(@RequestBody Task task){
+        return taskService.save(task);
+    }
+
+    @GetMapping("/status/{status}")
+    public List<Task> getByStatus(@PathVariable TaskStatus status){
+        return taskService.findByStatus(status);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletById(@PathVariable Long id){
+         taskService.deleteById(id);
     }
 
 
